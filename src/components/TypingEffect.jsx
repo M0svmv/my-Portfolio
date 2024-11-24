239ce59933db54fd9f57a0b70072785a1418e1ba
Mohammed Osama <131412392+M0svmv@users.noreply.github.com>
2024-11-24 20:42:09 +0200

    Update TypingEffect.jsx

--- src/components/TypingEffect.jsx
+++ src/components/TypingEffect.jsx
@@ -1,20 +1,24 @@
 import React, { useState, useEffect } from "react";
 
 function TypingEffect({ text, speed = 100 }) {
-  const [displayText, setDisplayText] = useState("");
+  const [displayText, setDisplayText] = useState([]);
 
   useEffect(() => {
     let index = 0;
     const interval = setInterval(() => {
-      if (index < text.length-1) {
-        setDisplayText((prev) => prev + text[index]);
+      if (index < text.length) {
+        if (text[index] === ".") {
+          setDisplayText((prev) => [...prev, <br key={index} />]); // Add a line break for "~"
+        } else {
+          setDisplayText((prev) => [...prev, text[index]]); // Add the character
+        }
         index++;
       } else {
         clearInterval(interval);
       }
     }, speed);
 
-    return () => clearInterval(interval); 
+    return () => clearInterval(interval);
   }, [text, speed]);
 
   return <span className="typing">{displayText}</span>;
